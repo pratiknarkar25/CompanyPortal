7948db56ef9ddaaa03b55f948cfec82f4d30160f
Pratik Narkar <pratiknarkar25@gmail.com>
2017-11-03 13:09:02 +0530

    Stoaring users email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,6 @@
 class SessionsController < ApplicationController
 	def create
-		p params
-		binding.pry
 	  user = User.from_omniauth(request.env["omniauth.auth"])
-		p '*' * 100
-		binding.pry
 		flash[:error] = "Not a valid Synerzip employee!" unless user 
 	  session[:user_id] = user&.id
 	  redirect_to root_path
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102124255) do
+ActiveRecord::Schema.define(version: 20171103071522) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "provider"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171102124255) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
