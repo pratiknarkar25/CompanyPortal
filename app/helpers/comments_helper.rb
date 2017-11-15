714ebd106c8994a8c4c04ef17bed461298868310
Pratik Narkar <pratiknarkar25@gmail.com>
2017-11-15 18:30:10 +0530

    Added nesting for the comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,12 @@
 
 # These methods will be used in comment views
 module CommentsHelper
+  def nested_messages(messages)
+    return if messages.blank?
+    messages.map do |message, sub_messages|
+      render(partial: 'comments/comment', locals: { message: message }) +
+        content_tag(:div, nested_messages(sub_messages),
+                    class: 'nested_messages')
+    end.join.html_safe
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@
 
 # Add database interactions for comments here
 class Comment < ApplicationRecord
+  has_ancestry
   belongs_to :commentable, polymorphic: true
   validates :content, presence: true
   belongs_to :user
