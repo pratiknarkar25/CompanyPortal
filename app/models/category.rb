43bf70f4c3e2b6611537ab08e9cd3c3b9d311128
Pratik Narkar <pratiknarkar25@gmail.com>
2017-11-28 20:09:36 +0530

    Homepage & other UI changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# This is a model for categories
 class Category < ApplicationRecord
-	validates :name, presence: true, uniqueness: true
-	has_many :posts, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  has_many :posts, dependent: :destroy
 end
