fbe1abd84749a78957fdfc759644f37979092948
Pratik Narkar <pratiknarkar25@gmail.com>
2018-01-08 19:11:14 +0530

    Added controller specs comments, sessions

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,43 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
+# rubocop:disable BlockLength
 RSpec.describe CommentsController, type: :controller do
+  describe 'POST create' do
+    it 'create a new comment' do
+      session[:user_id] = FactoryGirl.create(:admin_user).id
+      comment_flats_for_rent = FactoryGirl.create(:comment_flats_for_rent)
+      rent_post = comment_flats_for_rent.commentable
 
+      post :create, params:
+      {
+        category_id: rent_post.category_id, post_id: rent_post.id,
+        comment: { content: 'How much is rent' }
+      }
+      comment = assigns(:comment)
+      expect(comment.content).to eq 'How much is rent'
+      response.should redirect_to(category_post_path(rent_post.category_id,
+                                                     rent_post))
+    end
+  end
+
+  describe 'POST delete' do
+    it 'create a new comment' do
+      session[:user_id] = FactoryGirl.create(:admin_user).id
+      comment_flats_for_rent = FactoryGirl.create(:comment_flats_for_rent)
+      rent_post = comment_flats_for_rent.commentable
+
+      expect do
+        delete :destroy, params:
+        {
+          category_id: rent_post.category_id,
+          post_id: rent_post.id,
+          id: comment_flats_for_rent
+        }
+      end.to change(Comment, :count).by(-1)
+      response.should redirect_to(category_post_path(rent_post.category_id,
+                                                     rent_post))
+    end
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'POST create' do
+    it 'create a new session' do
+      user = FactoryGirl.create(:normal_user)
+      User.stub(:from_omniauth).and_return(user)
+      post :create, params: {}
+      expect(flash[:error]).to eq nil
+      expect(session[:user_id]).to eq user.id
+      response.should redirect_to(root_path)
+    end
+  end
 
+  describe 'DELETE destroy' do
+    it 'destroy a session' do
+      session[:user_id] = FactoryGirl.create(:admin_user).id
+      delete :destroy, params: { id: session[:user_id] }
+      expect(session[:user_id]).to eq nil
+      response.should redirect_to(root_path)
+    end
+  end
 end
